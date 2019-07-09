a4883e0ecb1592a51ab9a2e060d2e0abfef194c9
Tyrese Williams <tyresewilliams@Tyreses-MacBook-Pro.local>
2019-07-08 21:50:20 -0400

    components and inital context state

---
+++ src/context.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+const Context = React.createContext();
+
+export class Provider extends Component {
+    state = {
+        track_list: [
+            { track: { track_name: 'abc' } },
+            { track: { track_name: 'abc 123' } }
+
+        ],
+        heading: 'Top 10 Tracks'
+    };
+    componentDidMount() {
+        axios.get(``)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
+    }
+    render() {
+        return (
+            <Context.Provider value={this.state}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+export const Consumer = Context.Consumer;
